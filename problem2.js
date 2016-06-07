d74b8de275f437b3fb63d75ad74e08f9b8a70fdb
Stu Porter <sporter21@gmail.com>
2016-06-07 07:42:48 -0500

    p 1 and 2

---
+++ problem2.js
@@ -0,0 +1,10 @@
+
+function calculateSupply(age, food) {
+	return console.log('You will need ' + food + " to last until the ripe age of " + age )
+}
+
+calculateSupply('85', 'pickles');
+
+calculateSupply('62', 'hamburgers')
+
+calculateSupply('90', 'apples')
